5816fa96664bcfdeff9fdc53e508f6e7c754438f
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2023-01-16 17:02:16 -0500

    part 2 Deleting items from the todos

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -6,11 +6,12 @@ class TodosList extends React.Component {
     return (
       <ul>
         {this.props.todos.map(todo => (
-         <TodoItem 
-         key={todo.id} 
+         <TodoItem
+         key={todo.id}
          todo={todo}
          handleChangeProps={this.props.handleChangeProps}
-         />
+         deleteTodoProps={this.props.deleteTodoProps}
+       />
         ))}
       </ul>
     )
