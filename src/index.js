ac75cf3abe92b729c62133049b64826c4ba8387e
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2023-01-17 07:19:26 -0500

    Part 8 - Add svg icons

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import ReactDOM from "react-dom"
 //component file
 import TodoContainer from "./functionBased/components/TodoContainer"
-
 //stylesheet
 import "./functionBased/App.css"
 ReactDOM.render(
